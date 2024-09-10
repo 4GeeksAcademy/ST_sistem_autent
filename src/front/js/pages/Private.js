98f3932b537551df4373ae58803f5145851bb6b1
StevenSanz <163450220+StevenSanz@users.noreply.github.com>
2024-09-10 10:37:23 +0000

    stupdates

--- src/front/js/pages/Private.js
+++ src/front/js/pages/Private.js
@@ -1,10 +1,17 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Context } from "../store/appContext";
-
+import { useNavigate } from "react-router-dom"; // Para redirigir al usuario
 
 export const Private = () => {
-	const { store, actions } = useContext(Context);
+    const { store } = useContext(Context);
+    const navigate = useNavigate();
 
+    // Si no hay token, redirige al usuario al login
+    useEffect(() => {
+        if (!store.token) {
+            navigate("/login"); // Redirigir a la página de login
+        }
+    }, [store.token, navigate]);
 	return (
 		<div className="container">
 			<h1>Private view</h1>
